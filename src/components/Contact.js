19d367ee30aac0fd60bd664487f889849c92824c
dj.arsenovic@gmail.com <dj.arsenovic@gmail.com>
2018-08-04 22:33:38 +0200

    Unused div wrapper turned to ReactFragment, and Contact component got button to show and hide ul list

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -10,6 +10,9 @@ import PropTypes from 'prop-types';
  */
 
 class Contact extends Component {
+  state = {
+    showContactInfo: true
+  };
   static propTypes = {
     name: PropTypes.string.isRequired,
     email: PropTypes.string.isRequired,
@@ -22,24 +25,30 @@ class Contact extends Component {
     phone: 'Phone not inserted.'
   };
 
-  onShowClick = e => {
-    console.log(e.target);
+  onShowClick = () => {
+    this.setState({ showContactInfo: !this.state.showContactInfo });
   };
 
   render() {
     const { name, email, phone } = this.props.contact;
+    const { showContactInfo } = this.state;
     return (
       <div className="card mb-3">
-        <div className="card-body">
-          <h4 className="card-title" onClick={this.onShowClick}>
-            {name}
-            <i className="fas fa-sort-down" />
-          </h4>
+        <div className="card-body p-2">
+          <h4 className="card-title d-inline">{name}</h4>
+          <a
+            onClick={this.onShowClick}
+            className="btn btn-success btn-sm text-white d-block float-right"
+          >
+            Show
+          </a>
+        </div>
+        {showContactInfo ? (
           <ul className="list-group">
             <li className="list-group-item">Email: {email}</li>
             <li className="list-group-item">Phone: {phone}</li>
           </ul>
-        </div>
+        ) : null}
       </div>
     );
   }
