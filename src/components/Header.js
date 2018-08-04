7d2acaf30c69335e0ac69a97ca4f5218fff8663f
dj.arsenovic@gmail.com <dj.arsenovic@gmail.com>
2018-08-04 16:51:30 +0200

    Added destructuring to header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,9 +2,10 @@
 import React from 'react';
 // Creates functional component
 const Header = props => {
+  const { branding } = props;
   return (
     <div>
-      <h1>{props.branding}</h1>
+      <h1>{branding}</h1>
     </div>
   );
 };
