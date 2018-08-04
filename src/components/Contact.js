9975f253fda7704bbd0d8566f31c9b0c4af30fdf
dj.arsenovic@gmail.com <dj.arsenovic@gmail.com>
2018-08-04 19:04:06 +0200

    Added onClick event

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -21,13 +21,18 @@ class Contact extends Component {
     email: 'Email not inserted.',
     phone: 'Phone not inserted.'
   };
-
+  onShowClick() {
+    console.log('clicked');
+  }
   render() {
     const { name, email, phone } = this.props.contact;
     return (
       <div className="card mb-3">
         <div className="card-body">
-          <h4 className="card-title">{name}</h4>
+          <h4 className="card-title">
+            {name}
+            <i onClick={this.onShowClick} className="fas fa-sort-down" />
+          </h4>
           <ul className="list-group">
             <li className="list-group-item">Email: {email}</li>
             <li className="list-group-item">Phone: {phone}</li>
