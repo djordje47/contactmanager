b00abd4b67053566187f22072d24a70883142f29
dj.arsenovic@gmail.com <dj.arsenovic@gmail.com>
2018-08-04 23:59:53 +0200

    Rewrite. A big one. Delete action rewrite to use the Context and Reducer

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,6 +4,7 @@
  */
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { Consumer } from '../context';
 /**
  * Creates new class component called Contact that extends React.Component
  * and has render method that renders User
@@ -16,8 +17,7 @@ class Contact extends Component {
   static propTypes = {
     name: PropTypes.string.isRequired,
     email: PropTypes.string.isRequired,
-    phone: PropTypes.string.isRequired,
-    deleteClickHandler: PropTypes.func.isRequired
+    phone: PropTypes.string.isRequired
   };
 
   static defaultProps = {
@@ -35,43 +35,51 @@ class Contact extends Component {
     this.setState({ showContactInfo: !this.state.showContactInfo });
   };
   // Delete contact
-  onClickDelete = e => {
-    this.props.deleteClickHandler();
+  onClickDelete = (id, dispatch) => {
+    console.log(id);
+    dispatch({ type: 'DELETE_CONTACT', payload: id });
   };
 
   render() {
-    const { name, email, phone } = this.props.contact;
+    const { id, name, email, phone } = this.props.contact;
     const { showContactInfo } = this.state;
     return (
-      <div className="card mb-3">
-        <div className="card-body p-2">
-          <h4 className="card-title d-inline">{name}</h4>
-          <div
-            className="btn-group float-right"
-            role="group"
-            aria-label="Basic example"
-          >
-            <a
-              onClick={this.onShowClick}
-              className="btn btn-info btn-sm text-white d-block float-right"
-            >
-              Show
-            </a>
-            <a
-              onClick={this.onClickDelete}
-              className="btn btn-danger btn-sm text-white d-block float-right"
-            >
-              Delete
-            </a>
-          </div>
-        </div>
-        {showContactInfo ? (
-          <ul className="list-group">
-            <li className="list-group-item">Email: {email}</li>
-            <li className="list-group-item">Phone: {phone}</li>
-          </ul>
-        ) : null}
-      </div>
+      <Consumer>
+        {value => {
+          const { dispatch } = value;
+          return (
+            <div className="card mb-3">
+              <div className="card-body p-2">
+                <h4 className="card-title d-inline">{name}</h4>
+                <div
+                  className="btn-group float-right"
+                  role="group"
+                  aria-label="Basic example"
+                >
+                  <a
+                    onClick={this.onShowClick}
+                    className="btn btn-info btn-sm text-white d-block float-right"
+                  >
+                    Show
+                  </a>
+                  <a
+                    onClick={this.onClickDelete.bind(this, id, dispatch)}
+                    className="btn btn-danger btn-sm text-white d-block float-right"
+                  >
+                    Delete
+                  </a>
+                </div>
+              </div>
+              {showContactInfo ? (
+                <ul className="list-group">
+                  <li className="list-group-item">Email: {email}</li>
+                  <li className="list-group-item">Phone: {phone}</li>
+                </ul>
+              ) : null}
+            </div>
+          );
+        }}
+      </Consumer>
     );
   }
 }
--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -1,54 +1,22 @@
 import React, { Component } from 'react';
 import Contact from './Contact';
-class Contacts extends Component {
-  constructor() {
-    // Calling super() method cause we are extending component
-    super();
-    // State
-    this.state = {
-      contacts: [
-        {
-          id: 1,
-          name: 'John Doe',
-          email: 'jdoe@gmail.com',
-          phone: '555-555-555'
-        },
-        {
-          id: 2,
-          name: 'Karren Williams',
-          email: 'kwilliams@gmail.com',
-          phone: '555-555-4444'
-        },
-        {
-          id: 3,
-          name: 'Henry Johnson',
-          email: 'h.johnson@gmail.com',
-          phone: '123-555-555'
-        }
-      ]
-    };
-  }
-  deleteContact = id => {
-    // Pull contacts from the state
-    const { contacts } = this.state;
-    // Search and add not equal contacts to newContacts
-    const newContacts = contacts.filter(contact => contact.id !== id);
-    // Update state with new contacts
-    this.setState({ contacts: newContacts });
-  };
+import { Consumer } from '../context';
 
+class Contacts extends Component {
   render() {
-    const { contacts } = this.state;
     return (
-      <React.Fragment>
-        {contacts.map(contact => (
-          <Contact
-            key={contact.id}
-            contact={contact}
-            deleteClickHandler={this.deleteContact.bind(this, contact.id)}
-          />
-        ))}
-      </React.Fragment>
+      <Consumer>
+        {value => {
+          const { contacts } = value;
+          return (
+            <React.Fragment>
+              {contacts.map(contact => (
+                <Contact key={contact.id} contact={contact} />
+              ))}
+            </React.Fragment>
+          );
+        }}
+      </Consumer>
     );
   }
 }
