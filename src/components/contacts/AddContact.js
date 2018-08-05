5609e9bf7f4ad580086460072bcc13ff7a7f1595
dj.arsenovic@gmail.com <dj.arsenovic@gmail.com>
2018-08-05 15:43:23 +0200

    Created form with initial state. This is now controlbased component it does not let us populate the form, we need to create onChange event that will sync both the state and the form.

--- src/components/contacts/AddContact.js
+++ src/components/contacts/AddContact.js
@@ -1,10 +1,56 @@
 import React, { Component } from 'react';
 
 class AddContact extends Component {
+  state = {
+    name: '',
+    email: '',
+    phone: ''
+  };
   render() {
+    const { name, email, phone } = this.state;
     return (
-      <div>
-        <h1>AddContact</h1>
+      <div className="card mb-3">
+        <div className="card-header">AddContact</div>
+        <div className="card-body">
+          <form>
+            <div className="form-group">
+              <label htmlFor="name">Name</label>
+              <input
+                type="text"
+                className="form-control form-control-lg"
+                placeholder="Enter name"
+                name="name"
+                value={name}
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="email">Name</label>
+              <input
+                type="email"
+                className="form-control form-control-lg"
+                placeholder="Enter email"
+                name="email"
+                value={email}
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="phone">Phone</label>
+              <input
+                type="text"
+                className="form-control form-control-lg"
+                placeholder="Enter name"
+                name="phone"
+                value={phone}
+              />
+            </div>
+            <input
+              type="submit"
+              name="submit"
+              value="Add Contact"
+              className="btn btn-light btn-block"
+            />
+          </form>
+        </div>
       </div>
     );
   }
