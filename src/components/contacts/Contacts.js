659976786428b2a0e64c6b666514a5b7a08608d8
dj.arsenovic@gmail.com <dj.arsenovic@gmail.com>
2018-08-08 20:07:45 +0200

    Small UI style changes.

--- src/components/contacts/Contacts.js
+++ src/components/contacts/Contacts.js
@@ -11,7 +11,8 @@ class Contacts extends Component {
           return (
             <React.Fragment>
               <h1 className="display-4 mb-2">
-                <span className="text-success">Contact</span> list
+                <span className="text-success">Contact</span>
+                <span className="text-secondary">List</span>
               </h1>
               {contacts.map(contact => (
                 <Contact key={contact.id} contact={contact} />
