24f8de509ffa091342bba90f10d9edc2be2e719e
dj.arsenovic@gmail.com <dj.arsenovic@gmail.com>
2018-08-08 19:52:25 +0200

    Removing params example

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -6,7 +6,6 @@ const About = props => {
       <h1 className="display-4">About contact manager</h1>
       <p className="lead">Simple app to manage contacts.</p>
       <p className="text-secondary">Version 1.0.0</p>
-      <p className="text-secondary">{props.match.params.id}</p>
     </div>
   );
 };
