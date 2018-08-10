0e138c319825f667b92c868de18154d1709634c2
dj.arsenovic@gmail.com <dj.arsenovic@gmail.com>
2018-08-10 12:06:58 +0200

    Added post request with axios

--- src/components/contacts/AddContact.js
+++ src/components/contacts/AddContact.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import { Consumer } from './../../context';
 import TextInputGroup from './../layout/TextInputGroup';
-import uuid from 'uuid';
+import axios from 'axios';
+// import uuid from 'uuid';
 
 class AddContact extends Component {
   state = {
@@ -18,14 +19,17 @@ class AddContact extends Component {
     if (this.validateInputs(name, email, phone)) {
       // Create new contact
       const newContact = {
-        id: uuid(),
         name,
         email,
         phone
       };
-
       // Save contact
-      dispatch({ type: 'ADD_CONTACT', payload: newContact });
+      axios
+        .post('https://jsonplaceholder.typicode.com/users', newContact)
+        .then(res => {
+          dispatch({ type: 'ADD_CONTACT', payload: res.data });
+          console.log(res);
+        });
 
       // Clear contact form
       this.setState({
